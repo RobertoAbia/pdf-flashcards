2d54c9ed7cf6f426958cc7d32c5c65c924143037
RobertoAbia <robertoabia2@gmail.com>
2024-12-26 12:19:49 +0100

    fix: corregir importación de MiniTimer

--- src/components/TimerWrapper.tsx
+++ src/components/TimerWrapper.tsx
@@ -1,13 +1,14 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
-import { MiniTimer } from './MiniTimer';
+import MiniTimer from './MiniTimer';
 
 export const TimerWrapper = () => {
   const pathname = usePathname();
-  const showMiniTimer = pathname !== '/pomodoro';
 
-  if (!showMiniTimer) return null;
-  
+  if (!pathname.startsWith('/dashboard')) {
+    return null;
+  }
+
   return <MiniTimer />;
 };
